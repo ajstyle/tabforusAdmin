5eef0aad7429437f36d0ded75fa0b1a9371d8b32
Amit jain <amitjain.lov@gmail.com>
2017-11-16 12:56:10 +0530

    change package.json

--- src/app/you-tube/add-channel/add-channel.component.ts
+++ src/app/you-tube/add-channel/add-channel.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import {MatDialog, MatDialogRef} from '@angular/material';
-import { FormBuilder, FormGroup, Validators ,FormControl} from '@angular/forms';
+import { MatDialog, MatDialogRef } from '@angular/material';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-add-channel',
@@ -9,33 +9,35 @@ import { FormBuilder, FormGroup, Validators ,FormControl} from '@angular/forms';
   encapsulation: ViewEncapsulation.None
 })
 export class AddChannelComponent implements OnInit {
-form:FormGroup;
+  form: FormGroup;
   constructor(public dialogRef: MatDialogRef<AddChannelComponent>) { }
 
   ngOnInit() {
-  this.form = new FormGroup({
- ChannelName : new FormControl('', [Validators.required , Validators.minLength(3) ]),
- ChannelId : new FormControl('', [Validators.required, Validators.minLength(24)  ])
- })
+    this.form = new FormGroup({
+      ChannelName: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      ChannelId: new FormControl('', [Validators.required, Validators.minLength(24)])
+    })
   }
-  onCancel(){
+  onCancel() {
     this.dialogRef.close();
   }
-  save(){
-   this.dialogRef.close(this.form.value);
+  save() {
+    this.dialogRef.close(this.form.value);
   }
 
-   getChannelErrorMessage() {
-if(!this.form.controls["ChannelName"].touched)
-  return false;
-else
-return this.form.get('ChannelName').hasError('required') ? 'Please Enter Channel Id' :
-       this.form.get('ChannelName').hasError('minlength') ? 'Channel Name too sort' : ''
+  /*********************Validation****************/
+
+  getChannelErrorMessage() {
+    if (!this.form.controls["ChannelName"].touched)
+      return false;
+    else
+      return this.form.get('ChannelName').hasError('required') ? 'Please Enter Channel Id' :
+        this.form.get('ChannelName').hasError('minlength') ? 'Channel Name too sort' : ''
 
   }
- getChannelIdErrorMessage() {
-return this.form.get('ChannelId').hasError('required') ? 'Please Enter Channel Name' :
-       this.form.get('ChannelId').hasError('minlength') ? 'Not a vaild Channel ID' :''
-}
+  getChannelIdErrorMessage() {
+    return this.form.get('ChannelId').hasError('required') ? 'Please Enter Channel Name' :
+      this.form.get('ChannelId').hasError('minlength') ? 'Not a vaild Channel ID' : ''
+  }
 
 }
--- src/app/you-tube/you-tube.component.ts
+++ src/app/you-tube/you-tube.component.ts
@@ -1,14 +1,14 @@
 
-import { Component, OnInit, ViewEncapsulation , ViewContainerRef } from '@angular/core';
-import { FormBuilder, FormGroup,FormGroupDirective, NgForm, Validators ,FormControl} from '@angular/forms';
+import { Component, OnInit, ViewEncapsulation, ViewContainerRef } from '@angular/core';
+import { FormBuilder, FormGroup, FormGroupDirective, NgForm, Validators, FormControl } from '@angular/forms';
 import { ApiService } from '../api.service';
-import {MatTableDataSource} from '@angular/material';
-import {ChannelListInterface} from '../channel-list' ;
-import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
-import {AddChannelComponent} from './add-channel/add-channel.component';
+import { MatTableDataSource } from '@angular/material';
+import { ChannelListInterface } from '../channel-list';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { AddChannelComponent } from './add-channel/add-channel.component';
 import { ToastsManager } from 'ng2-toastr/ng2-toastr';
-import {MatSnackBar} from '@angular/material';
-import {ErrorStateMatcher} from '@angular/material/core';
+import { MatSnackBar } from '@angular/material';
+import { ErrorStateMatcher } from '@angular/material/core';
 
 
 
@@ -18,116 +18,126 @@ import {ErrorStateMatcher} from '@angular/material/core';
   styleUrls: ['./you-tube.component.css'],
   encapsulation: ViewEncapsulation.None
 })
-export class YouTubeComponent implements OnInit  {
-   channelList ;
-dataSource ;
-data ;
-value ;
-form;
-show:boolean = false ;
+export class YouTubeComponent implements OnInit {
 
-channelIdControl = new FormControl('', [
+  channelList;
+  dataSource;
+  data;
+  value;
+  form;
+  show: boolean = false;
+
+
+  /* ----------------------- Validation of Update Field ---------------------*/
+  channelIdControl = new FormControl('', [
     Validators.required,
- Validators.minLength(24)
+    Validators.minLength(24)
   ]);
-ChannelNameControl = new FormControl('', [
+  ChannelNameControl = new FormControl('', [
     Validators.required,
- Validators.minLength(3)
+    Validators.minLength(3)
   ]);
   matcher = new MyErrorStateMatcher();
 
-  displayedColumns = ['S.no.', 'channelName', 'channelId' , 'Action'];
 
-  constructor(public dialog: MatDialog,private ApiService : ApiService , public snackBar: MatSnackBar ) {
 
-  }
 
-  /* Get Channel */
+  /* ----------------------- Table Coloumns ---------------------*/
 
-   ngOnInit() {
-this.show = true ;
-     this.ApiService.getChannel().subscribe((channelList) => {
-             this.channelList   = channelList
-             console.log(  this.channelList ) ;
-             this.dataSource = new MatTableDataSource<ChannelListInterface>(this.channelList);
-     this.show = false ;
-            });
+  displayedColumns = ['S.no.', 'channelName', 'channelId', 'Action'];
+
+  constructor(public dialog: MatDialog, private ApiService: ApiService, public snackBar: MatSnackBar) {
 
   }
 
-  /* Save Channel */
+  /* ----------------------- GEt Channel ---------------------*/
+
+  ngOnInit() {
+    this.show = true;
+    this.ApiService.getChannel().subscribe((channelList) => {
+      this.channelList = channelList
+      console.log(this.channelList);
+      this.dataSource = new MatTableDataSource<ChannelListInterface>(this.channelList);
+      this.show = false;
+    });
+
+  }
+
+  /* ----------------------- Save Channel ---------------------*/
   save(channel) {
 
-this.show = true ;
+    this.show = true;
 
-this.ApiService.saveChannel( channel).subscribe((data)=>{
+    this.ApiService.saveChannel(channel).subscribe((data) => {
 
-  this.data  =  data
-this.channelList.push(this.data.result) ;
-console.log(this.channelList);
+      this.data = data
+      this.channelList.push(this.data.result);
+      console.log(this.channelList);
 
-this.dataSource = new MatTableDataSource<ChannelListInterface>(this.channelList);
-console.log(this.form);
-this.show = false
+      this.dataSource = new MatTableDataSource<ChannelListInterface>(this.channelList);
+      console.log(this.form);
+      this.show = false
 
-  this.snackBar.open('Channel Added' ,'',  {
-  duration: 2000
-});
-})
-
-
-  }
-
-
-
-  update(id , channelname , channelId ) {
-this.show = true ;
-    this.value = { } ;
-console.log(channelId);
-    this.value.ChannelName = channelname ;
-    this.value.ChannelId = channelId ;
-
-  this.ApiService.update(id,this.value).subscribe(()=>{
-
-
-     this.dataSource = new MatTableDataSource<ChannelListInterface>(this.channelList);
-   this.show = false ;
-    this.snackBar.open('Channel Update' ,'',  {
-  duration: 2000
-});
+      this.snackBar.open('Channel Added', '', {
+        duration: 2000
+      });
     })
   }
+
+
+  /************ Update Channel ****************/
+
+  update(id, channelname, channelId) {
+    this.show = true;
+    this.value = {};
+    console.log(channelId);
+    this.value.ChannelName = channelname;
+    this.value.ChannelId = channelId;
+
+    this.ApiService.update(id, this.value).subscribe(() => {
+
+
+      this.dataSource = new MatTableDataSource<ChannelListInterface>(this.channelList);
+      this.show = false;
+      this.snackBar.open('Channel Update', '', {
+        duration: 2000
+      });
+    })
+  }
+
+
+  /************ Delete Channel ****************/
   delete(id) {
-    this.show = true ;
-   this.ApiService.delete(id).subscribe(()=>{
-     console.log("delete "+id) ;
-   this.channelList = this.channelList.filter( (channel) => { return channel._id != id  }  ) ;
-   console.log(this.channelList);
-     this.dataSource = new MatTableDataSource<ChannelListInterface>(this.channelList);
-   this.show = false ;
-       this.snackBar.open('Channel Delete','',  {
-  duration: 2000
-});
+    this.show = true;
+    this.ApiService.delete(id).subscribe(() => {
+      console.log("delete " + id);
+      this.channelList = this.channelList.filter((channel) => { return channel._id != id });
+      console.log(this.channelList);
+      this.dataSource = new MatTableDataSource<ChannelListInterface>(this.channelList);
+      this.show = false;
+      this.snackBar.open('Channel Delete', '', {
+        duration: 2000
+      });
     })
   }
 
-  addChannel(){
-let dialogRef = this.dialog.open(AddChannelComponent, {  });
+  /************ Add Channel Dialog Box****************/
+
+  addChannel() {
+    let dialogRef = this.dialog.open(AddChannelComponent, {});
     dialogRef.afterClosed().subscribe(channel => {
       console.log('The dialog was closed');
-      if(channel){
+      if (channel) {
         console.log(channel);
-          this.save(channel);
+        this.save(channel);
       }
     });
   }
 
 }
 
-  /* ----------------------- Validation ---------------------*/
-
-
 /** Error when invalid control is dirty, touched, or submitted. */
+
 export class MyErrorStateMatcher implements ErrorStateMatcher {
   isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
     const isSubmitted = form && form.submitted;
